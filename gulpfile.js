1c308f87bb9c5cfb23f3f1da8f6a76bd02be9fb5
yansilva89 <yansilva89@gmail.com>
2017-06-01 18:41:13 -0300

    configuração das tasks e instalação de plugins gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var watch = require('gulp-watch');
 
-gulp.task('default', function(){
-   
+//Task de Excução do SASS WATCH
+gulp.task('watch', function(){
+    gulp.watch('sass/**/*.sass', ['sass'])
 });
+
+//Task de Excução do SASS
+gulp.task('sass', function(){
+    return sass ('sass/**/*.sass').pipe(gulp.dest('css'))//.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)).pipe(gulp.dest('./css'));
+});
+
+//Task de Execução do GULP
+gulp.task('default', ['sass', 'watch']);
 
